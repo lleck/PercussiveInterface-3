41b5ca64aac5a5cab88e559524e7d5986ca701f9
lleck <lleck@gmx.net>
2023-06-29 18:58:33 +0200

    differential reading of hall sensors

--- percussiveInterface-3.1/percussiveInterface-3.1.ino
+++ percussiveInterface-3.1/percussiveInterface-3.1.ino
@@ -1,3 +1,5 @@
+#include <ADC.h>
+#include <ADC_util.h>
 /* Analog Input Example, Teensyduino Tutorial #4
    http://www.pjrc.com/teensy/tutorial4.html
 
@@ -13,31 +15,47 @@
    This example code is in the public domain.
 */
 
+ADC *adc = new ADC(); // adc object;
+
 void setup()
 {                
   Serial.begin(38400);
-    pinMode(A14, INPUT);
-    pinMode(A15, INPUT);
-    pinMode(A16, INPUT);
+    pinMode(A10, INPUT);
+    pinMode(A11, INPUT); //ref 1.65
+    
+    pinMode(A12, INPUT);
+    pinMode(A13, INPUT); //ref 1.65
+    
+    adc->adc0->setAveraging(64); // set number of averages
+    adc->adc0->setResolution(16); // set bits of resolution
+    adc->adc0->setConversionSpeed(ADC_CONVERSION_SPEED::VERY_LOW_SPEED); // change the conversion speed
+    adc->adc0->setSamplingSpeed(ADC_SAMPLING_SPEED::MED_SPEED); // change the sampling speed
+
+    adc->adc1->setAveraging(64); // set number of averages
+    adc->adc1->setResolution(16); // set bits of resolution
+    adc->adc1->setConversionSpeed(ADC_CONVERSION_SPEED::VERY_LOW_SPEED); // change the conversion speed
+    adc->adc1->setSamplingSpeed(ADC_SAMPLING_SPEED::MED_SPEED); // change the sampling speed
+   
 }
 float val1;
 float val2;
-float val3;
+
 
 void loop()                     
 {
-  val1 = analogRead(A14);
-  val2 = analogRead(A15);
-  val3 = analogRead(A16);
+
+ 
+ 
+  val1 = adc->adc0->analogReadDifferential(A10, A11); 
+  val2 = adc->adc1->analogReadDifferential(A12, A13); 
+
 
   Serial.print("val1\t");
   Serial.print(val1);
   Serial.print("    ");
   Serial.print("val2\t");
-  Serial.print(val2);
-  Serial.print("    ");
-  Serial.print("val3\t");
-  Serial.println(val3);
+  Serial.println(val2);
+
   
   delay(5);
 }
