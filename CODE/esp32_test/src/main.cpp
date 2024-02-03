2ac1c8ce69efcfbdcf2ac600ccb7bc839c1c6a06
lleck <lleck@gmx.net>
2024-02-03 12:56:49 +0100

    restored from 01.02

--- CODE/esp32_test/src/main.cpp
+++ CODE/esp32_test/src/main.cpp
@@ -19,15 +19,15 @@
 
 #define LED_CLOCK_PIN 12
 #define LED_DATA_PIN 11
+#define LED_CS_PIN -1
+#define colorSaturation 128
 #define CLOCK_PIN 47
 #define MOSI_PIN 48
 #define MISO_PIN 14
 #define MUX1_SYNC_PIN 1
 #define MUX2_SYNC_PIN 9
 #define TMAG_CS_PIN 21
-#define IR_VIBROMETER_PIN 13 // QRE 1113
-#define IR_SENSOR_PIN 6      // Rotation Interrupt
-// #define IO35 35
+#define IR_VIBROMETER_PIN 13
 
 // Globals
 const uint8_t pixelCount = 52;
@@ -37,91 +37,65 @@ const uint16_t angularDivisions = 360;
 uint8_t currentSensor = 0;
 // Zähler für die aktuelle angulare Division
 uint16_t numDiv = 0;
-
 // two 2D array buffers for the magnetic sensors (left & right arm)
-int magneticArray1[sensorCount*2][angularDivisions] ;
-int magneticArray2[sensorCount*2][angularDivisions] ;
+float magneticArray1[sensorCount][angularDivisions];
+float magneticArray2[sensorCount][angularDivisions];
 
-// Channel select commands for ADG731 Mux (center first)
-uint8_t mux1_ch[26] = {30, 31, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 16, 17, 18, 19, 20, 21, 22, 23};
-uint8_t mux2_ch[26] = {23, 22, 21, 20, 19, 18, 17, 16, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 12, 13, 14, 15, 31, 30};
+// Channel select commands for ADG731 Mux
+uint8_t ch_select_cmd[34] = {0x80,0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F,0x10,0x11,0x12,0x13,0x14,0x15,0x16,0x17,0x18,0x19,0x1A,0x1B,0x1C,0x1D,0x1E,0x1F,0x40};
 
-uint8_t led_ch[52] = {51, 0, 50, 1, 49, 2, 48, 3, 47, 4, 46, 5, 45, 6, 44, 7, 43, 8, 42, 9, 41, 10, 40, 11, 39, 12, 38, 13, 37, 14, 36, 15, 35, 16, 34,
-                      17, 33, 18, 32, 19, 31, 20, 30, 21, 29, 22, 28, 23, 27, 24, 26, 25};
 // init the TMAG5170 Class
 TMAG5170 magneticSensor;
 // init the NeoPixelBus instance with Spi and alternate Pins
-NeoPixelBus<DotStarBgrFeature, DotStarMethod> strip(pixelCount, LED_CLOCK_PIN, LED_DATA_PIN);
+NeoPixelBus<DotStarBgrFeature, DotStarSpi40MhzMethod> strip(pixelCount);
 
-uint8_t colorSaturation = 5;
 RgbColor red(colorSaturation, 0, 0);
 RgbColor green(0, colorSaturation, 0);
-RgbColor blue(0, 0, colorSaturation);
+RgbColor blue(0, 0, colorSaturation);       
 RgbColor white(colorSaturation);
 RgbColor black(0);
 
-// forward declaring functions as this is not written in Arduino IDE
-void sensorChannel(uint8_t muxNr, uint8_t channelNr);
-void mux_off(int muxNr);
+//forward declaring functions as this is not written in Arduino IDE
+void sensorChannel (uint8_t muxNr, uint8_t channelNr);
 void readPosition();
-int readMagneticSensor();
+float readMagneticSensor();
 
-SPISettings muxSPI(1000000, MSBFIRST, SPI_MODE2); // spi configuration for the ADG731
+SPISettings muxSPI(10000000, MSBFIRST, SPI_MODE2); // spi configuration for the ADG731
 
-void setup()
-{
+
+
+void setup(){
   D_SerialBegin(9600);
-  delay(2000);
   SPI.begin(CLOCK_PIN, MISO_PIN, MOSI_PIN);
   pinMode(MUX1_SYNC_PIN, OUTPUT); // set the SS pin as an output
   pinMode(MUX2_SYNC_PIN, OUTPUT); // set the SS pin as an output
   pinMode(TMAG_CS_PIN, OUTPUT);
-  pinMode(IR_SENSOR_PIN, INPUT);
   digitalWrite(MUX1_SYNC_PIN, HIGH);
   digitalWrite(MUX2_SYNC_PIN, HIGH);
-  digitalWrite(TMAG_CS_PIN, HIGH);
+  digitalWrite(TMAG_CS_PIN, HIGH);        
 
   bool error = false;
-  // start the strip on the defined SPI bus and init to black = all pixels off
-  strip.Begin();
-  strip.ClearTo(black); // this resets all the DotStars to an off state
-  strip.Show();
+  //  // start the strip on the defined SPI bus and init to black = all pixels off
+  // strip.Begin(LED_CLOCK_PIN, LED_DATA_PIN, LED_DATA_PIN, LED_CS_PIN);
+  // strip.ClearTo(black); // this resets all the DotStars to an off state
+  // strip.Show();
+
+
 
   magneticSensor.begin(TMAG_CS_PIN);
   magneticSensor.disable_crc();
 
-  for (int i = 0; i < sensorCount && !error; i++)
+  for (int i = 1; i <= sensorCount && !error; i++)
   {
     sensorChannel(1, i);
-    delay(1);
     magneticSensor.default_cfg(&error);
-    delay(1);
-    strip.SetPixelColor(led_ch[i], red);
-    strip.Show();
-    delay(50);
-    strip.SetPixelColor(led_ch[i], black);
-    strip.Show();
   }
-  mux_off(1);
-  strip.ClearTo(black); // this resets all the DotStars to an off state
-  strip.Show();
-
-  for (int j = 0; j < sensorCount && !error; j++)
+  
+  for (int j = 1; j <= sensorCount && !error; j++)
   {
     sensorChannel(2, j);
-    delay(1);
-    magneticSensor.default_cfg(&error);
-    delay(1);
-    strip.SetPixelColor(led_ch[j + sensorCount], red);
-    strip.Show();
-    delay(50);
-    strip.SetPixelColor(led_ch[j + sensorCount], black);
-    strip.Show();
+    magneticSensor.default_cfg(&error);  
   }
-  mux_off(2);
-
-  strip.ClearTo(black); // this resets all the DotStars to an off state
-  strip.Show();
 
   if (error)
   {
@@ -129,171 +103,109 @@ void setup()
     D_println(currentSensor);
   }
 
+  // send a command to set all Channels Off
+  // sensorChannel(1, 0);
+  // sensorChannel(2, 0);
+
   D_println("TMAG_config_complete");
+
 }
 
-void sensorChannel(uint8_t muxNr, uint8_t channelNr)
-{
-  if (muxNr == 1){
-    SPI.beginTransaction(muxSPI);
-    digitalWrite(MUX1_SYNC_PIN, LOW);
-    SPI.transfer(mux1_ch[channelNr]); // send a command to select channel
-    digitalWrite(MUX1_SYNC_PIN, HIGH);
-    SPI.endTransaction();
-  }
-  else if (muxNr == 2){
-    SPI.beginTransaction(muxSPI);
-    digitalWrite(MUX2_SYNC_PIN, LOW);
-    SPI.transfer(mux2_ch[channelNr]); // send a command to select channel
-    digitalWrite(MUX2_SYNC_PIN, HIGH);
-    SPI.endTransaction();
-  }
-  // switch (muxNr)
-  // {
-  // case 1:
-  //   SPI.beginTransaction(muxSPI);
-  //   digitalWrite(MUX1_SYNC_PIN, LOW);
-  //   SPI.transfer(mux1_ch[channelNr]); // send a command to select channel
-  //   digitalWrite(MUX1_SYNC_PIN, HIGH);
-  //   SPI.endTransaction();
-  //   //break;
-  // case 2:
-  //   SPI.beginTransaction(muxSPI);
-  //   digitalWrite(MUX2_SYNC_PIN, LOW);
-  //   SPI.transfer(mux2_ch[channelNr]); // send a command to select channel
-  //   digitalWrite(MUX2_SYNC_PIN, HIGH);
-  //   SPI.endTransaction();
-  //   //break;
-  // default:
-  //   break;
-  // }
-}
-
-void mux_off(int muxNr)
-{
-  
-  if (muxNr == 1)
-  { 
-    SPI.beginTransaction(muxSPI);
-    digitalWrite(MUX1_SYNC_PIN, LOW);
-    SPI.transfer(0x80);
-    digitalWrite(MUX1_SYNC_PIN, HIGH);
-    SPI.endTransaction();
-  }
-  else if (muxNr == 2)
+void sensorChannel (uint8_t muxNr, uint8_t channelNr) 
+{   
+  switch (muxNr)
   {
-    SPI.beginTransaction(muxSPI);
-    digitalWrite(MUX2_SYNC_PIN, LOW);
-    SPI.transfer(0x80);
-    digitalWrite(MUX2_SYNC_PIN, HIGH);
-    SPI.endTransaction();
+    case 1:
+      SPI.beginTransaction(muxSPI);
+      digitalWrite(MUX1_SYNC_PIN, LOW);
+      SPI.transfer(ch_select_cmd[channelNr]);      // send a command to select channel 
+      currentSensor = channelNr;                   // keep track of current sensor for debugging
+      digitalWrite(MUX1_SYNC_PIN, HIGH);
+      SPI.endTransaction();
+    //break;
+    case 2:
+      SPI.beginTransaction(muxSPI);
+      digitalWrite(MUX2_SYNC_PIN, LOW);
+      SPI.transfer(ch_select_cmd[channelNr]);      // send a command to select channel 
+      currentSensor = channelNr + sensorCount;     // keep track of current sensor for debugging
+      digitalWrite(MUX2_SYNC_PIN, HIGH);
+      SPI.endTransaction();
+    //break;
+    default: break;
+    
   }
-  
 }
 
 void readPosition()
 {
-  for (int sensorIndex = 0; sensorIndex < 51; sensorIndex++)
+  for (int sensorIndex = 0; sensorIndex < sensorCount; sensorIndex++)
   {
-    //bei geraden Indexzahlen
-    if (sensorIndex % 2 == 0)
-    {
-      sensorChannel(1, sensorIndex/2);
-      // Read from the SPI-controlled sensor
-      int sensorValue1 = readMagneticSensor();
-      D_print(sensorValue1);
-      D_print("\t");
-      // Store the sensor value in the array
-      magneticArray1[sensorIndex][numDiv] = sensorValue1;
-      mux_off(1);
-
-      // Select the appropriate channel on the second multiplexer
-      sensorChannel(2, sensorIndex/2);
-      // Read from the SPI-controlled sensor
-      int sensorValue2 = readMagneticSensor();
-      D_println(sensorValue2);
-      // Store the sensor value in the array
-      magneticArray2[sensorIndex][numDiv] = sensorValue2;
-      mux_off(2);
-
-       
-    }
-   
-    //bei ungeraden Indexzahlen
-    else
-    {
-      magneticArray1[sensorIndex][numDiv] = (magneticArray1[sensorIndex+1][numDiv] + magneticArray1[sensorIndex-1][numDiv]) / 2;
-      magneticArray2[sensorIndex][numDiv] = (magneticArray2[sensorIndex+1][numDiv] + magneticArray2[sensorIndex-1][numDiv]) / 2;
-    }
- 
-
+    // Select the appropriate channel on the first multiplexer
+    sensorChannel(1, sensorIndex);
+    // Read from the SPI-controlled sensor
+    float sensorValue1 = readMagneticSensor();
+    // Store the sensor value in the array
+    magneticArray1[sensorIndex][numDiv] = sensorValue1;
+    sensorChannel(1, 0);
+    
+    // Select the appropriate channel on the second multiplexer
+    sensorChannel(2, sensorIndex);
+    // Read from the SPI-controlled sensor
+    float sensorValue2 = readMagneticSensor();
+    // Store the sensor value in the array
+    magneticArray2[sensorIndex][numDiv] = sensorValue2;
+    sensorChannel(2, 0);
     // // Print the sensor index and values
-    // D_print("Sensor ");
-    // D_print(sensorIndex + 1);  // Sensor index starts from 1
-    // D_print(": ");
-    // D_print("MUX1 - ");
-    // D_print(sensorValue1);
-    // D_print(", MUX2 - ");
-    // D_println(sensorValue2);
+    D_print("Sensor ");
+    D_print(sensorIndex + 1);  // Sensor index starts from 1
+    D_print(": ");
+    D_print("MUX1 - ");
+    D_print(sensorValue1);
+    D_print(", MUX2 - ");
+    D_println(sensorValue2);
   }
   // numDiv++;
   // if (numDiv >= angularDivisions)
-  numDiv = 0;
+  //   numDiv = 0;
 }
 
-int readMagneticSensor()
+float readMagneticSensor()
 {
   bool error = false;
-  int value = magneticSensor.getZresult(&error);
+  float value = magneticSensor.getZresult(&error);
   if (error)
   {
-    D_print("error reading sensor nr. ");
-    D_println(currentSensor);
-    delay(10);
+   D_print("error reading sensor nr. ");
+   D_println(currentSensor);
+   delay(10);
   }
   return value;
 }
 
-void loop()
-{
-  sensorChannel(1, 1);
-  int sensorValue1 = readMagneticSensor();
-  D_println(sensorValue1);
-  delay(200);
-  sensorChannel(1, 2);
-  int sensorValue2 = readMagneticSensor();
-  D_println(sensorValue2);
-  delay(200);
-  mux_off(1);
-  delay(200);
-  sensorChannel(2, 1);
-  int sensorValue3 = readMagneticSensor();
-  D_println(sensorValue3);
-  delay(200);
-  mux_off(2);
-  delay(200);
-  // for (int j = 0; j < sensorCount*2; j++)
-  // {
-  //   if (magneticArray1[j][0] > 200)
-  //   {
-  //     colorSaturation = map(magneticArray1[j][0], 0, 32768, 0, 40);
-  //     strip.SetPixelColor(led_ch[j], RgbColor(0, colorSaturation, 0));
-  //   }
-  //   else
-  //   {
-  //     strip.SetPixelColor(led_ch[j], black);
-  //   }
-  //   strip.Show();
-  // }
-  // // int rotVal = analogRead(IR_SENSOR_PIN);
-  // // D_println(rotVal);
+void loop(){
+    
+    // readPosition();
+     //magneticSensor.simple_read(TMAG5170_REG_CONV_STATUS);
+    // // Select the channel on the first multiplexer
 
-
-    // // u_long start_time = micros();
-   //readPosition();
-  // // u_long end_time = micros();
-  // // u_long duration = end_time - start_time;
-  // // D_println(duration);
-  //  delay(50);
+     sensorChannel(2, 4);
+     D_println("mux2 ch 4");
+     float sensorValue2 = readMagneticSensor();
+     D_println(sensorValue2);
+     delay(1000);
+    sensorChannel(2, 0);
+    // sensorChannel(1, 0);
+     D_println("mux2 off");
+     delay(200);
+     sensorChannel(1, 4);
+     D_println("mux1 ch 4");
+     float sensorValue1 = readMagneticSensor();
+     D_println(sensorValue1);
+     delay(1000);
+    sensorChannel(1, 0 );
+      //sensorChannel(2, 0);
+     D_println("mux1 off");
+     delay(200);
 
 }
+
